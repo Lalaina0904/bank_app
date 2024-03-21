356ad6ef02cd70f54bc69fb8b508c2124ac886bb
Lalaina0904 <lalainap19.aps2b@gmail.com>
2024-03-21 11:36:00 +0300

    feat: separate form in another componennt

--- app/newbie/page.tsx
+++ app/newbie/page.tsx
@@ -1,90 +1,9 @@
-"use client";
-import * as React from "react";
+import NewAccountForm from "@/components/newAccountForm";
+import React from "react";
 
-import { useState } from "react";
-
-import Link from "next/link";
-
-import {
-    Briefcase,
-    BookText,
-    MessagesSquare,
-    Search,
-    LogOut,
-    FileText,
-    DownloadCloud,
-    Upload,
-    UploadCloud,
-} from "lucide-react";
-
-import Image from "next/image";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form";
-
-import { Input } from "@/components/ui/input";
-
-import { toast } from "@/components/ui/use-toast";
-
-const formSchema = z.object({
-    firstName: z.string(),
-    lastName: z.string(),
-    dateOfBirth: z.string(),
-    salary: z.number(),
-    accountID: z.string(),
-});
-
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
-
-export default function Newbie() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            firstName: "",
-            lastName: "",
-            dateOfBirth: "",
-            salary: 0,
-            accountID: "",
-        },
-    });
-
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
-
-        toast({
-            title: "You submitted the following values:",
-            description: (
-                <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
-                    <code className='text-white'>
-                        {JSON.stringify(values, null, 2)}
-                    </code>
-                </pre>
-            ),
-        });
-    }
-
+const page = () => {
     return (
-        <div className='flex justify-center h-screen mt-10'>
+        <div className='flex justify-center mt-10'>
             <div className='relative flex flex-col gap-6 max-w-2xl w-full'>
                 <h2 className='font-medium text-xl pt-4'>
                     Welcome in your Banking app ✨
@@ -96,104 +15,11 @@ export default function Newbie() {
                 </p>
 
                 <div>
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className='w-2/3 space-y-6'
-                        >
-                            <FormField
-                                control={form.control}
-                                name='firstName'
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>First name</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                type='text'
-                                                placeholder='John'
-                                                {...field}
-                                            />
-                                        </FormControl>
-
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name='lastName'
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Last name</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                type='text'
-                                                placeholder='Doe'
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name='dateOfBirth'
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Date of birth</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                type='date'
-                                                placeholder='Doe'
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name='salary'
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Salary</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                type='number'
-                                                placeholder='0'
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name='accountID'
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Account ID</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                type='text'
-                                                placeholder='0'
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <Button type='submit' className='px-6'>
-                                Submit
-                            </Button>
-                        </form>
-                    </Form>
+                    <NewAccountForm />
                 </div>
             </div>
         </div>
     );
-}
+};
+
+export default page;
